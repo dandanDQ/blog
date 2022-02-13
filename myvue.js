ed9a632b6bf88e9139497d6b609921227e5cb478
dandanDQ <shendanqiong@qq.com>
2022-02-13 22:12:50 +0800

    feat: update

--- myvue.js
+++ myvue.js
@@ -57,10 +57,8 @@ function genVnode(tmpl) {
 
 function parseAttributes(node, rawTag) {
   const tag = rawTag.slice(1, -1)
-
-  // 不能使用空格作为分割符
-  // 忽略双引号内的空格 quetos
-  // const attributes = tag.slice(1, -1).split(' ').slice(1).filter((attr) => attr.trim())
+  // 不能使用空格作为分割符，因为需要忽略双引号内的空格 quetos
+  // 错误写法：const attributes = tag.slice(1, -1).split(' ').slice(1).filter((attr) => attr.trim())
 
   let inQuotes = false // 是否出现一个未闭合的引号
   let attr = ''
@@ -74,19 +72,18 @@ function parseAttributes(node, rawTag) {
       } else {
         // 过滤掉空的 attr
         attr.trim() && attributes.push(attr.trim())
-        inQuotes = false
         attr = '' // 清空
       }
     } else {
       attr += char
-      if(char === '"') {
+      if(char === '"') { // 遇到 " 符号，切换 inQuotes 状态
         inQuotes = !inQuotes
       }
     }
   }
   // 存入最后一个结果
   attr.trim() && attributes.push(attr.trim())
-  
+  // 将 attributes 列表存入 node.attributes 属性中
   for(const attr of attributes) {
     if(attr.indexOf('=') === -1) continue
     const key = attr.split('=')[0]
@@ -96,49 +93,36 @@ function parseAttributes(node, rawTag) {
   return node
 }
 
-function matchMustache(w, realDom, track) {
-  // 匹配每一对 {{}}，并将其中的字符作为 key，匹配并替换 data 中对应 key 的值
-  // /\{\{(.*)\}\}/g 这个不行！
-  let word = w
-  const exp = /\{\{(.+?)\}\}/g
-  const matchs = word.match(exp) ?? []
-  // 然后还是用正则的替换咯？
-  for(const match of matchs) {
-    const key = match.slice(2,-2).trim()
-    // 需要收集依赖，当 key 对应的值更新时，调用 diff 算法更新 dom
-
-    if(track) {
-      realDomTarget = realDom
-      wTarget = w
-    }
-    // 使 mustache 支持表达式，点操作
-    function getVal(){
-      let res = new Function(`with(data) { return ${key}}`)()
-      if(typeof res === 'object'){
-        res = JSON.stringify(res)
-      }
-      return res
-    }
-    const value = getVal()
-    if(track) {
-      // 全局变量归位
-      realDomTarget = null
-      wTarget = ''
-    }
-    word = word.replace(match, value)
-  }
-  return word
-}
-
-// 根据 vnode 进行渲染
 function getType(target) {
   return Object.prototype.toString.call(target).slice(8, -1)
 }
 
-function createOneNode (vnode, track = true) {
-  // 如果是文本节点，可能是空的
-  const tag = vnode.tagName
+function createRealDom(vnode) {
+  // 可能是对象，也可能是数组
+  let realDom = undefined
+  if(getType(vnode) === 'Array') {
+    realDom = []
+    for(const item of vnode) {
+      realDom.push(createRealDom(item))
+    }
+  } else {
+    // 加入 v-for 指令后，这里可能返回一个列表
+    realDom = createOneNode(vnode)
+    if(vnode.children) {
+      // children 返回的是一个列表
+      const childlist = createRealDom(vnode.children)
+      for(const child of childlist) {
+        realDom.appendChild(child)
+      }
+    }
+  } 
+  return realDom
+}
 
+function createOneNode (vnode) {
+  // 如果是文本节点，可能没有 tag
+  
+  const tag = vnode.tagName
   if(tag) {
     const node = document.createElement(tag)
     // 将 class 加上
@@ -167,40 +151,52 @@ function createOneNode (vnode, track = true) {
 
   const node = document.createTextNode(vnode)
   // 文本节点，可能涉及到 mustache 转换了;如果在这里转化 mustache 语法，会不会更好收集依赖
-  const replaced = matchMustache(vnode, node, track)
+  
+  realDomTarget = node
+  wTarget = vnode
+
+  const replaced = matchMustache(vnode)
   node.nodeValue = replaced
+
+  // 全局变量归位
+  realDomTarget = null
+  wTarget = ''
+  
   return node
 }
 
-function createRealDom(vnode) {
-  // 可能是对象，也可能是数组
-  let realDom = undefined
-  if(getType(vnode) === 'Array') {
-    realDom = []
-    for(const item of vnode) {
-      realDom.push(createRealDom(item))
-    }
-  } else {
-    // 加入 v-for 指令后，这里可能返回一个列表
-    realDom = createOneNode(vnode)
-    if(vnode.children) {
-      // children 返回的是一个列表
-      const childlist = createRealDom(vnode.children)
-      for(const child of childlist) {
-        realDom.appendChild(child)
+function matchMustache(w) {
+  // 匹配每一对 {{}}，并将其中的字符作为 key，匹配并替换 data 中对应 key 的值
+  // /\{\{(.*)\}\}/g 这个不行！
+  let word = w
+  const exp = /\{\{(.+?)\}\}/g
+  const matchs = word.match(exp) ?? []
+  // 然后还是用正则的替换咯？
+  for(const match of matchs) {
+    const key = match.slice(2,-2).trim()
+
+    // 使 mustache 支持表达式，点操作
+    function getVal(){
+      let res = new Function(`with(data) { return ${key}}`)()
+      if(typeof res === 'object'){
+        res = JSON.stringify(res)
       }
+      return res
     }
-  } 
-  return realDom
+    const value = getVal()
+
+    word = word.replace(match, value)
+  }
+  return word
 }
 
 let realDomTarget = null
 let wTarget = ''
 
 function reactiveData(rawData) {
-  // 将数据转化为响应式
+  // 递归地将数据转化为响应式
   const handler = {
-    get(obj, property, receiver) {
+    get(obj, property) {
       track(property)
       return obj[property]
     },
@@ -212,81 +208,64 @@ function reactiveData(rawData) {
     }
   }
 
-  function track(property) {
-    if(typeof property === 'string' && realDomTarget) {
-      
-        if(!Dep[property]) {
-          Dep[property] = []
-        }
-        // 使用 map 减少重复操作
-        let target = domTargetMap.get(realDomTarget)
-        if(!target) {
-          target = { w: wTarget,realDom: realDomTarget }
-          domTargetMap.set(realDomTarget, target)
-        }
-        // 而且是没存储过的依赖
-        if(Dep[property].indexOf(target) === -1) {
-          Dep[property].push(target)
-        }
-    }
-  }
-
-  function notify(property) {
-    if(Dep[property]) {
-        const deps = Dep[property].slice()
-        // Dep[property] = []
-        // dom 和 data 是多对多的关系，更新一个，必须更新其他
-        for(const dep of deps) {
-          const oldNode = dep.realDom
-          const newNode = createOneNode(dep.w, false) // 这里就不收集依赖了
-
-          // 更新nodevalue
-          oldNode.nodeValue = newNode.nodeValue
-          // oldNode.parentNode.replaceChild(newNode, oldNode)
-          // more：如果依赖被删除，需要移除对应的节点
-        }
-      }
-  }
-
-  const data = new Proxy(rawData, handler)
   function deepProxy(data) {
-    for(const key in data) {
-      const val = data[key]
+    const newData = new Proxy(data, handler)
+    for(const key in newData) {
+      const val = newData[key]
       if(typeof val === 'object') {
-        data[key] = new Proxy(val, handler)
-        deepProxy(data[key])
+        newData[key] = deepProxy(newData[key])
       }
     }
+    return newData
   }
-  deepProxy(data)
-  return data
+  return deepProxy(rawData)
+}
+
+function track(property) {
+  if(typeof property === 'string' && realDomTarget) {
+      if(!Dep[property]) {
+        Dep[property] = []
+      }
+      // 使用 map 减少重复操作
+      let target = domTargetMap.get(realDomTarget)
+      if(!target) {
+        target = { w: wTarget,realDom: realDomTarget }
+        domTargetMap.set(realDomTarget, target)
+      }
+      // 而且是没存储过的依赖
+      if(Dep[property].indexOf(target) === -1) {
+        Dep[property].push(target)
+      }
+  }
+}
+
+function notify(property) {
+  if(Dep[property]) {
+      const deps = Dep[property].slice()
+      // Dep[property] = []
+      for(const dep of deps) {
+        const realDom = dep.realDom
+        const replaced = matchMustache(dep.w)
+        // 更新nodevalue
+        realDom.nodeValue = replaced
+      }
+    }
 }
 
 let data = undefined
+let methods = undefined
 const Dep = {} // 用于存放依赖的对象
 const domTargetMap = new Map()
-class dep {
-  constructor() {
-    this.subs = []
-  }
-  addSub(sub) {
-    this.subs.push(sub)
-  }
-  notify() {
-    this.subs.forEach((sub) => {
-      sub.update()
-    })
-  }
-}
 
 function myVue(options) { 
+  // 转成全局变量只是为了便于获取，减少代码量
   data = reactiveData(options.data())
   methods = options.methods
   this.data = data
   // 获取模板，编译为虚拟 dom
-
   const tmpl = options.template[0] === '#' ?  document.querySelector(options.template).innerHTML : options.template
-  const vnode = genVnode(tmpl) // 要把这些文本数据，转化并创建为真实的dom树，append到应用根节点上
+  // 把html字符串转化虚拟dom
+  const vnode = genVnode(tmpl)
   // 使用虚拟dom创建真实dom树
   const fragment = document.createDocumentFragment() // fragment用来接收所有的结果，之后统一挂载到应用根节点上
   const realDom = createRealDom(vnode) 
